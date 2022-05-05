4563bfd78a2a76b5dd6364ad87f70d6c64337db6
Timo Scarabel <scarabelt@gmail.com>
2022-05-05 09:14:28 +0200

    Created device controller

---
+++ src/main/java/com/example/mygpstrackerapi/controllers/DeviceController.java
@@ -0,0 +1,26 @@
+package com.example.mygpstrackerapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/devices")
+public class DeviceController {
+
+	@GetMapping()
+	public String getDevices() {
+		return "Devices";
+	}
+
+	@GetMapping("/{id}")
+	public String getDeviceById(@PathVariable String id) {
+		return "Device with id: " + id;
+	}
+
+	@GetMapping("/tag/{tag}")
+	public String getDevicesByTag(@PathVariable String tag) {
+		return "Devices with tag: " + tag;
+	}
+}
